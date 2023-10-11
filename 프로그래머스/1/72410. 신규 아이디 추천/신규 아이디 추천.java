f4df94830186b9f1cf83a3171f4ec8471af46386
Anna <76768480+chaeanna@users.noreply.github.com>
2023-10-11 15:48:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.69 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String new_id) {
+        // 1단계
+        new_id = new_id.toLowerCase();
+        // 2단계
+        new_id = new_id.replaceAll("[^-_.a-z0-9]", "");
+        // 3단계
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        // 4단계
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
+        // 5단계
+        if (new_id.isEmpty()) {
+            new_id = "a";
+        }
+        // 6단계
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+            if (new_id.endsWith(".")) {
+                new_id = new_id.replaceAll("[.]$","");
+            }
+        } 
+        // 7단계
+        if (new_id.length() <= 2) {
+            while (new_id.length() < 3) {
+                new_id += new_id.charAt(new_id.length() - 1);
+            }
+        }
+        
+        
+        return new_id;
+    }
+}
