fd073c920056fe9f1a557e3589924635c420810d
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-06 23:28:19 +0900

    [level 1] Title: 문자열 나누기, Time: 0.50 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char x = s.charAt(0);
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (count == 0) {
+                x = s.charAt(i);
+            }
+            if (x == s.charAt(i)) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) answer++;
+        }
+        
+        if (count > 0) {
+            answer++;
+        }
+        return answer;
+    }
+}
