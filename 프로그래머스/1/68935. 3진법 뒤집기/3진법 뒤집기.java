b86f3c1d3e86c0787fd628a999c7ebc9c6ed20cb
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-17 21:55:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,10 +1,15 @@
 class Solution {
     public int solution(int n) {
+        int answer = 0;
+        
+        // 10진수 -> 3진법
+        String n3 = String.valueOf(Integer.toString(n, 3));
         StringBuilder sb = new StringBuilder();
-        String str = Integer.toString(n, 3);
-        sb.append(str);
-        sb.reverse();
-        int answer = Integer.parseInt(sb.toString(), 3);
+        sb.append(n3);
+        
+        // 앞뒤 반전 후 10진법으로 표현
+        answer = Integer.parseInt(sb.reverse().toString(), 3);
+        
         return answer;
     }
 }
