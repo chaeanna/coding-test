1201d2e762b0534b65e85d1a25ba1ce386ccdef5
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-18 16:38:46 +0900

    [level 1] Title: 시저 암호, Time: 1.39 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,22 +1,33 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, int n) {
-        char[] str = s.toCharArray();
+        // char로 분리
+        char[] ch = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for (char st : str) {
-            if (st != ' ') {
-                int num = (int) st+ n;
-                if (num > 122) {
-                    num -= 26;
-                } else if ((int) st <= 90 && num > 90) {
-                    num -= 26;
-                } else if (num > 90 && num < 97) {
-                    num -= 26;
+        
+        for (int i = 0; i < ch.length; i++) {
+            char c = ch[i];
+            // n만큼 민 인덱스 번호
+            int idx = c + n;
+            
+            if ('A' <= c && 'Z' >= c) {
+                // 민 인덱스 번호가 'Z'보다 클 경우 -26
+                if (idx > 'Z') {
+                    idx -= 26;
                 }
-                st = (char) num;
+                
+            } else if ('a' <= c && 'z' >= c) {
+                // 민 인덱스 번호가 'z'보다 클 경우 -26
+                if (idx > 'z') {
+                    idx -= 26;
+                }
+            } else {
+                // 공백일 경우
+                sb.append(" ");
+                continue;
             }
-            sb.append(st);
+            // 문자 저장
+            sb.append(String.valueOf((char) idx));
+            
         }
         return sb.toString();
     }
