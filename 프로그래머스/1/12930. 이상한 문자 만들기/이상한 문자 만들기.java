ef4dbcbb2e9c83ff661088096cd19ea130fa8551
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-18 15:55:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.91 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,23 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] str = s.split("");
+        String[] str = s.toLowerCase().split("");
         int count = 0;
-        
-        for (String n : str) {
-            if (n.contains(" ")) {
-                count = 0;
-            } else {
+        for (int i = 0; i < str.length; i++) {
+            if (!str[i].equals(" ")) {
                 count++;
-            }
-            
-            if (count % 2 == 0) {
-                answer += n.toLowerCase();
+                if (count % 2 != 0) {
+                    str[i] = str[i].toUpperCase();
+                }
             } else {
-                answer += n.toUpperCase();
+                count = 0;
             }
         }
         
+        for (String n : str) {
+            answer += n;
+        }
         return answer;
     }
 }
