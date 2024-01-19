7ac924f28275154f6dd3880f59020fb485bb4d27
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-19 14:54:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,50 +1,14 @@
 class Solution {
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        char[] c = s.toCharArray();
-        for (int i = 0; i < c.length; i++) {
-            if (c[i] >= '0' && c[i] <= '9') {
-                sb.append(c[i]);
-            } else {
-                if (c[i] == 'z') {
-                    sb.append("0");
-                    i += 3;
-                } else if (c[i] == 'o') {
-                    sb.append("1");
-                    i += 2;
-                } else if (c[i] == 't') {
-                    if (c[i+1] == 'w') {
-                        sb.append("2");
-                        i += 2;
-                    } else {
-                        sb.append("3");
-                        i += 4;
-                    }
-                } else if (c[i] == 'f') {
-                    if (c[i+1] == 'o') {
-                        sb.append("4");
-                        i += 3;
-                    } else {
-                        sb.append("5");
-                        i += 3;
-                    }
-                } else if (c[i] == 's') {
-                    if (c[i+1] == 'i') {
-                        sb.append("6");
-                        i += 2;
-                    } else {
-                        sb.append("7");
-                        i += 4;
-                    }
-                } else if (c[i] == 'e') {
-                    sb.append("8");
-                    i += 4;
-                } else if (c[i] == 'n') {
-                    sb.append("9");
-                    i += 3;
-                }
+        String[] num = {"zero", "one", "two", "three", "four",
+                       "five", "six", "seven", "eight", "nine"};
+        
+        for (int i = 0; i < num.length; i++) {
+            if (s.contains(num[i])) {
+                s = s.replace(num[i], String.valueOf(i));
             }
         }
-        return Integer.parseInt(sb.toString());
+        
+        return Integer.parseInt(s);
     }
 }
