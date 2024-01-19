79a8892131dc3481fff6b1982f22005012fc2d69
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-19 15:45:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.32 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,19 +1,20 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-
+        
         for (int i = 0; i < n; i++) {
-            String str1 = String.format("%0"+n+"d", Long.parseLong(Integer.toBinaryString(arr1[i])));
-            String str2 = String.format("%0"+n+"d", Long.parseLong(Integer.toBinaryString(arr2[i])));
-            String result = "";
-            for (int j = 0; j < n; j++) {
-                if (str1.charAt(j) == '0' && str2.charAt(j) == '0') {
-                    result += " ";
-                } else {
-                    result += "#";
-                }
+            // OR 연산으로 둘 중 하나가 1이면 1
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            // 0은 " "으로 변환
+            answer[i] = answer[i].replaceAll("0", " ");
+            // 1은 "#"으로 변환
+            answer[i] = answer[i].replaceAll("1", "#");
+            
+            // 자릿수가 n일 때까지 문자열 앞에 공백 더하기
+            while (true) {
+                if (answer[i].length() >= n) break;
+                answer[i] = ' ' + answer[i];
             }
-            answer[i] = result;
         }
         return answer;
     }
