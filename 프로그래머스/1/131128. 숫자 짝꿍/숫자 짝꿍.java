d43d0c015a545c1433f6669b116d75e20a2d5872
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-22 18:02:22 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,26 +1,23 @@
 class Solution {
     public String solution(String X, String Y) {
-        String answer = "";
-        int[] Xcount = new int[10];
-        int[] Ycount = new int[10];
-        
+        int[] xcount = new int[10];
+        int[] ycount = new int[10];
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < X.length(); i++) {
-            Xcount[X.charAt(i) - '0']++;
+            xcount[X.charAt(i) - '0']++;
         }
         for (int i = 0; i < Y.length(); i++) {
-            Ycount[Y.charAt(i) - '0']++;
+            ycount[Y.charAt(i) - '0']++;
         }
-        
-        StringBuilder sb = new StringBuilder();
         for (int i = 9; i >= 0; i--) {
-            for (int j = 0; j < Math.min(Xcount[i], Ycount[i]); j++) {
+            for (int j = 0; j < Math.min(xcount[i], ycount[i]); j++) {
                 sb.append(i);
             }
         }
         
-        if (sb.length() < 1) {
+        if (sb.toString().equals("")) {
             return "-1";
-        } else if (sb.toString().charAt(0) == '0') {
+        } else if(sb.toString().charAt(0) == '0') {
             return "0";
         } else {
             return sb.toString();
