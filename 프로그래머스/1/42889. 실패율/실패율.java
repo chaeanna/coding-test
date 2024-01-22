bd4c5b9d4c92aebe97ed72b6d1939e69a3416a0e
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-22 16:06:19 +0900

    [level 1] Title: 실패율, Time: 1.00 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,31 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int N, int[] stages) {
-        HashMap<Integer, Double> stage = new HashMap<>();
-        // i번 스테이지
-        for (int i = 0; i < N; i++) {
-            int fail = 0;
-            int users = 0;
-            // 도전 현황
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        // 스테이지에 도달한 유저 수
+        int count = stages.length;
+        
+        HashMap<Integer, Double> map = new HashMap<>();
+        
+        for (int i = 1; i <= N; i++) {
+            int user = 0;
             for (int j = 0; j < stages.length; j++) {
-                if(i+1 == stages[j]) {
-                    fail++;
-                }
-                if (i+1 <= stages[j]) {
-                    users++;
+                // 클리어 못한 유저 수
+                if (i == stages[j]) {
+                    user++;
                 }
             }
-            if (users == 0) {
-                stage.put(i+1, 0.0);
+            
+            // 유저가 없으면 실패율 0
+            if (user == 0) {
+                map.put(i, (double) 0);
             } else {
-                stage.put(i+1, (double) fail / users);
+                map.put(i, (double) user/count);
+                count -= user;
             }
             
         }
-        List<Integer> answer = new ArrayList<>(stage.keySet());
-        Collections.sort(answer, (v1, v2) -> Double.compare(stage.get(v2), stage.get(v1)));
         
+        List<Integer> list = new ArrayList<>(map.keySet());
+        // 키의 value값을 기준으로 내림차순 정렬
+        Collections.sort(list, (v1, v2) -> Double.compare(map.get(v2), map.get(v1)));
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
         return answer;
     }
 }
