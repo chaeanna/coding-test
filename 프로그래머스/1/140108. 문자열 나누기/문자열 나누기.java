9caad9a8f15d4a384590a53b66a1f5b0d39df5cd
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-22 18:16:14 +0900

    [level 1] Title: 문자열 나누기, Time: 0.88 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,19 +3,29 @@ class Solution {
         int answer = 0;
         char x = s.charAt(0);
         int count = 0;
-
+        
         for (int i = 0; i < s.length(); i++) {
+            
+            // 새롭게 시작할 경우 x 글자 선언
             if (count == 0) {
                 x = s.charAt(i);
             }
+            
+            // 같은 글자일 경우 횟수 증가
             if (x == s.charAt(i)) {
                 count++;
             } else {
+                // 다른 글자일 경우 횟수 감소
                 count--;
             }
-            if (count == 0) answer++;
+            
+            // 횟수가 같아지는 순가
+            if (count == 0) {
+                answer++;
+            }
         }
-
+        
+        // 만일 마지막 글자가 남아있을 경우 횟수 증가
         if (count > 0) {
             answer++;
         }
