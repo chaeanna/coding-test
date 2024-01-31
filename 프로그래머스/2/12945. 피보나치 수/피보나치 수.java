2b420eae95a35fef0f5029e22da1cc69f96e6b94
Anna <76768480+chaeanna@users.noreply.github.com>
2024-01-31 20:29:25 +0900

    [level 2] Title: 피보나치 수, Time: 2.92 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,16 @@
 class Solution {
-    public static int[] fibo;
+    static int[] fibo = new int[100001];
     public int solution(int n) {
-        int answer = 0;
-        fibo = new int[n+1];
-        fibo[0] = 0;
-        fibo[1] = 1;
         fibonacci(n);
-        answer = fibo[n] % 1234567;
-        return answer;
+        return fibo[n];
     }
-    public void fibonacci(int n) {
-        for (int i = 2; i <= n; i++) {
-            fibo[i] = fibo[i-1] % 1234567 + fibo[i-2] % 1234567;
+    
+    public static void fibonacci(int n) {
+        fibo[0] = 0;
+        fibo[1] = fibo[2] = 1;
+        for (int i = 3; i < 100001; i++) {
+            fibo[i] = (fibo[i-2] + fibo[i-1]) % 1234567;
         }
+        
     }
 }
