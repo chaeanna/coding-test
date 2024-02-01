28b25842cee6dde94bc62420abefa9c360bce439
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-01 14:25:30 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-        
+        int answer = 0;
+
         while (n != 0) {
             if (n % 2 == 0) {
                 n /= 2;
             } else {
                 n -= 1;
-                ans++;
+                answer++;
             }
         }
 
-        return ans;
+        return answer;
     }
 }
