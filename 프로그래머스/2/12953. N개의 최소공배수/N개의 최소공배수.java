0aec03a5a2460696859d3d06be23a63573d7c2ae
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-02 23:06:45 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,19 +1,23 @@
 class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
-        
         for (int i = 1; i < arr.length; i++) {
             answer = lcm(answer, arr[i]);
         }
-        
+
         return answer;
     }
-    public static int gcd(int a , int b) {
-        if (a % b == 0) return b;
-        return gcd(b, a%b);
+    
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
     }
     
-    public static int lcm(int a, int b) {
+    public int lcm(int a, int b) {
         return a * b / gcd(a, b);
     }
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953#) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.02 ms
+메모리: 74.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 21:9:18
+2024년 2월 5일 23:6:43
 
 ### 문제 설명
 
