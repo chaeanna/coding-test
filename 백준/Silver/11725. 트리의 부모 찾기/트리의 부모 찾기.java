60eaeee595a0e1d1b4ce943a4d44bf0ca0fb623d
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-06 23:09:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 588 ms, Memory: 73320 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -6,18 +6,19 @@ import java.util.StringTokenizer;
 
 class Main {
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
     static int[] parents;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<Integer>());
+            graph.add(new ArrayList<>());
         }
-        for (int i = 0; i < N-1; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
@@ -25,36 +26,24 @@ class Main {
             graph.get(b).add(a);
         }
 
-        visited = new boolean[N+1];
-        parents = new int[N+1];
-        DFS(1);
+        parents = new int[N + 1];
+        DFS(1, -1); // 시작 노드는 1, 시작 노드의 부모는 없으므로 -1로 설정
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
         for (int i = 2; i <= N; i++) {
-            System.out.println(parents[i]);
+            sb.append(parents[i]).append('\n');
         }
+        System.out.print(sb);
     }
 
-    public static void DFS(int v) {
-        if (v != 1 && graph.get(v).size() == 1) {
-            int n = graph.get(v).get(0);
-            parents[v] = n;
-        } else {
-            visited[v] = true;
-            for (int i : graph.get(v)) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    DFS(i);
-                    visited[i] = false;
-                }
-            }
-            if (v == 1) return;
-            for (int i : graph.get(v)) {
-                if (visited[i]) {
-                    parents[v] = i;
-                    visited[v] = false;
-                    return;
-                }
+    public static void DFS(int v, int parent) {
+        parents[v] = parent; // 현재 노드의 부모 노드 설정
+
+        for (int child : graph.get(v)) {
+            if (child != parent) { // 부모 노드는 다시 방문하지 않도록 제외
+                DFS(child, v); // 재귀 호출
             }
         }
-
     }
 }
