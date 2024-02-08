b365ea3f0797ad724f0639f57391c47f47f7086e
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-08 18:24:40 +0900

    [level 2] Title: 귤 고르기, Time: 60.07 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,29 +4,25 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < tangerine.length; i++) {
-            if (map.containsKey(tangerine[i])) {
-                map.put(tangerine[i], map.get(tangerine[i]) + 1);
+        for (int i : tangerine) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
             } else {
-                map.put(tangerine[i], 1);
+                map.put(i, 1);
             }
         }
         
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            list.add(entry.getValue());
-        }
-        
+        List<Integer> list = new ArrayList<>(map.values());
         Collections.sort(list, Collections.reverseOrder());
+        
         int sum = 0;
-        for (int i = 0; i < list.size(); i++) {
+        for (Integer i : list) {
+            sum += i;
+            answer++;
             if (sum >= k) {
                 break;
             }
-            
-            sum += list.get(i);
-            answer++;
         }
         
         return answer;
