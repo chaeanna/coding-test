f6ee6497fb9004f6a8839ab07e9184d4c49a07e2
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-08 18:08:02 +0900

    [level 2] Title: 구명보트, Time: 8.34 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,16 +5,14 @@ class Solution {
         int answer = 0;
         Arrays.sort(people);
         int idx = 0;
-        
-        for (int i = people.length-1; i >= idx; i--) {
-            if (people[i] + people[idx] <= limit) {
+        for (int i = people.length - 1; i >= idx; i--) {
+            if (people[idx] + people[i] <= limit) {
                 idx++;
                 answer++;
             } else {
                 answer++;
             }
         }
-        
         return answer;
     }
 }
