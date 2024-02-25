c413d088bd4cf4819f0106d46bd49fa1513d6351
Anna <76768480+chaeanna@users.noreply.github.com>
2024-02-25 19:12:32 +0900

    [Gold V] Title: 적록색약, Time: 156 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,94 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
-class Point {
-    int y, x;
-    public Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
 class Main {
-    static char[][] graph, graphRb;
+    static int n, normalCnt, cnt;
+    static char[][] graph;
     static int[] disX = {1, -1, 0, 0};
     static int[] disY = {0, 0, 1, -1};
-    static int n, col, ncol = 0;
-    static Queue<Point> q = new LinkedList<>();
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
         graph = new char[n][n];
-        graphRb = new char[n][n];
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < n; j++) {
                 graph[i][j] = str.charAt(j);
-                graphRb[i][j] = str.charAt(j);
+            }
+        }
+
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    DFS(i, j);
+                    normalCnt++;
+                }
+            }
+        }
+
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == 'G') {
+                    graph[i][j] = 'R';
+                }
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (graph[i][j] != 'N') {
-                    BFS(i, j, graph[i][j]);
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    DFS(i, j);
+                    cnt++;
                 }
             }
         }
 
-        System.out.print(col + " " + ncol);
+        System.out.println(normalCnt + " " + cnt);
+
+
 
     }
 
-    public static void BFS(int y, int x, char color) {
-        graph[y][x] = 'N';
-        q.offer(new Point(y, x));
+    public static void DFS(int y, int x) {
+        char color = graph[y][x];
+        for (int i = 0; i < 4; i++) {
+            int dx = x + disX[i];
+            int dy = y + disY[i];
 
-        if (color != 'B' && graphRb[y][x] != 'N') {
-            while (!q.isEmpty()) {
-                graphRb[y][x] = 'N';
-                int len = q.size();
-                Point p = q.poll();
-                for (int i = 0; i < len; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        int dx = p.x + disX[j];
-                        int dy = p.y + disY[j];
-
-                        if (dx >= 0 && dx < n && dy >= 0 && dy < n
-                                && graphRb[dy][dx] != 'B' && graphRb[dy][dx] != 'N') {
-                            graphRb[dy][dx] = 'N';
-                            q.offer(new Point(dy, dx));
-                        }
-                    }
-                }
+            if (dx >= 0 && dx < n && dy >= 0 && dy < n
+                    && graph[dy][dx] == color && !visited[dy][dx]) {
+                visited[dy][dx] = true;
+                DFS(dy, dx);
             }
-            ncol++;
-            q.offer(new Point(y, x));
-        }
 
-        while (!q.isEmpty()) {
-            int len = q.size();
-            Point p = q.poll();
-            for (int i = 0; i < len; i++) {
-                for (int j = 0; j < 4; j++) {
-                    int dx = p.x + disX[j];
-                    int dy = p.y + disY[j];
 
-                    if (dx >= 0 && dx < n && dy >= 0 && dy < n
-                            && graph[dy][dx] == color) {
-                        graph[dy][dx] = 'N';
-                        q.offer(new Point(dy, dx));
-                    }
-                }
-            }
         }
-        col++;
-        if (color == 'B') {
-            ncol++;
-        }
-
     }
 }
