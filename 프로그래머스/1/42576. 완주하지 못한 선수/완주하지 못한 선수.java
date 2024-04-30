200d23ca1662853cb5fc20c3ff0643c81a875b6e
Anna <76768480+chaeanna@users.noreply.github.com>
2024-04-30 22:59:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.60 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,16 +5,15 @@ class Solution {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
         for (String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
         for (String s : completion) {
-            map.put(s, map.getOrDefault(s, 0) - 1);
+            map.put(s, map.get(s)-1);
         }
-
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                answer = key;
-                break;
+        
+        for (String s : participant) {
+            if (map.get(s) != 0) {
+                answer = s;
             }
         }
         return answer;
