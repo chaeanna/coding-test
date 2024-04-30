767b460401adbf195a8e0c5bb7ecae6c792537fa
Anna <76768480+chaeanna@users.noreply.github.com>
2024-04-30 22:14:01 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,36 +1,32 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] check = new int[n+1];
-        // 잃어버린 학생
-        for (int l = 0; l < lost.length; l++) {
-            check[lost[l]]--;
-        }
-        // 여분이 있는 학생
-        for (int r = 0; r < reserve.length; r++) {
-            check[reserve[r]]++;
+        int[] student = new int[n+1];
+        for (int i : lost) {
+            student[i]--;
         }
         
-        boolean[] lend = new boolean[n+1];
+        for (int i : reserve) {
+            student[i]++;
+        }
+        
         for (int i = 1; i <= n; i++) {
-            if (check[i] < 0) {
-                if (check[i-1] > 0 && !lend[i-1]) {
-                    lend[i-1] = true;
-                    check[i]++;
-                } else if (i+1 <= n && check[i+1] > 0 && !lend[i+1]) {
-                    lend[i+1] = true;
-                    check[i]++;
+            if (student[i] < 0) {
+                if (i-1 > 0 && student[i-1] > 0) {
+                    student[i-1]--;
+                    student[i]++;
+                } else if (i+1 <= n && student[i+1] > 0) {
+                    student[i+1]--;
+                    student[i]++;
                 }
             }
         }
         
         for (int i = 1; i <= n; i++) {
-            if (check[i] >= 0) {
+            if (student[i] >= 0) {
                 answer++;
             }
         }
-        
-        
         return answer;
     }
 }
