7fd23faed2eafcf373c4b26b1928ab769c813b94
Anna <76768480+chaeanna@users.noreply.github.com>
2024-04-30 23:16:09 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,35 +3,33 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> q = new LinkedList<>();
+        int work = 0; // 작업진도
+        int time = 0; // 작업일수
         for (int i = 0; i < progresses.length; i++) {
-            // 남은 작업량 계산
-            int remain = 100 - progresses[i];
-            int day = remain / speeds[i];
-            if (remain % speeds[i] != 0) {
-                day++;
+            work = 100 - progresses[i];
+            time = (work / speeds[i]) + (work % speeds[i] == 0 ? 0 : 1);
+            q.offer(time);
+        }
+        
+        List<Integer> list = new ArrayList<>(); // 각각 배포되는 기능 개수
+        while (!q.isEmpty()) {
+            int deploy = q.poll();
+            int count = 1;
+            while (!q.isEmpty()) {
+                if (deploy >= q.peek()) {
+                    q.poll();
+                    count++;
+                } else {
+                    break;
+                }
             }
             
-            // 작업량 큐에 넣기
-            q.offer(day);
+            list.add(count);
         }
         
-        int max = q.poll();
-        int count = 1;
-        List<Integer> list = new ArrayList<>();
-        while (!q.isEmpty()) {
-            int num = q.poll();
-            if (max >= num) {
-                count++;
-            } else {
-                list.add(count);
-                count = 1;
-                max = num;
-            }
-        }
         
-        list.add(count);
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
         return answer;
