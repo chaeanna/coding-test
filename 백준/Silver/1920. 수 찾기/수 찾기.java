272209b9e486bf6e55d03fbe6892e82c1509c113
Anna <76768480+chaeanna@users.noreply.github.com>
2024-05-01 15:41:05 +0900

    [Silver IV] Title: 수 찾기, Time: 620 ms, Memory: 48084 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        HashMap<Integer, Integer> A = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            A.put(Integer.parseInt(st.nextToken()), 0);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (A.containsKey(num)) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
