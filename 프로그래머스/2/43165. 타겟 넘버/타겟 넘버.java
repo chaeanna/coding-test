985d01da0aeb08f5deb6b3137aded634b47985e6
Anna <76768480+chaeanna@users.noreply.github.com>
2024-05-01 14:29:14 +0900

    [level 2] Title: 타겟 넘버, Time: 0.54 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,18 @@
-import java.util.*;
-
 class Solution {
-    static int count, len, targetValue;
-    static int[] number;
-
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        len = numbers.length;
-        number = numbers;
-        targetValue = target;
-        DFS(0, 0);
-        return count;
+        DFS(numbers, target, 0, 0);
+        return answer;
     }
-    public static void DFS(int level, int idx) {
-        if (level == len) {
-            int sum = 0;
-            for (int i : number) {
-                sum += i;
+    public static void DFS(int[] numbers, int target, int start, int result) {
+        if (start > numbers.length-1) {
+            if (target == result) {
+                answer++;
             }
-            if (sum == targetValue) {
-                count++;
-            }
+            return;
         }
-        for (int i = idx; i < len; i++) {
-            DFS(level+1, i+1);
-            number[i] = number[i] * -1;
-            DFS(level+1, i+1);
-        }
+        DFS(numbers, target, start+1, result+numbers[start]);
+        DFS(numbers, target, start+1, result+(-1*numbers[start]));
+        
     }
 }
