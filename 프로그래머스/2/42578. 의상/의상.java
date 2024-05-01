0ac253cc3ed4dd8750b5ce55bfc0beed0fc5cb93
Anna <76768480+chaeanna@users.noreply.github.com>
2024-05-01 19:07:15 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,20 +4,17 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < clothes.length; i++) {
-            // 종류 가짓수
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0) + 1);
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
         }
         
-        // 각 종류의 개수
-        Iterator<Integer> it = map.values().iterator();
-        
-        while (it.hasNext()) {
-            // 안 입는 경우까지 포함해서 +1 하기
-            answer *= it.next().intValue() + 1;
+        List<String> list = new ArrayList<>(map.keySet());
+        for (int i = 0; i < list.size(); i++) {
+            int count = map.get(list.get(i)) + 1;
+            answer *= count;
         }
-
-        // 아무것도 안입었을 경우 -1
+        
+        
         return answer-1;
     }
 }
