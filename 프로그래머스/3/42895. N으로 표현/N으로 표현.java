d1d9488f65deb9654e485eeca1c190e25e458f9c
Anna <76768480+chaeanna@users.noreply.github.com>
2024-05-01 12:31:01 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -6,48 +6,43 @@ class Solution {
         if (N == number) {
             return 1;
         }
-        // 결과값 저장
-        List<Set<Integer>> dp = new LinkedList<>();
-        
-        // 최솟값 8까지, 저장할 공간 만들기
+        List<Set<Integer>> dp = new ArrayList<>();
         for (int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<>()); // 중복값 제거
+            dp.add(new HashSet<Integer>());
         }
         
-        dp.get(1).add(N); // 첫번째는 N 하나
+        dp.get(1).add(N);
         
         for (int i = 2; i <= 8; i++) {
-            
-            // 숫자 붙이기 (사칙연산 외에 ex. 5, 55, 555)
             StringBuilder sb = new StringBuilder().append(N);
+            // 숫자 이어붙이기
             for (int j = 1; j < i; j++) {
                 sb.append(N);
             }
             
             dp.get(i).add(Integer.parseInt(sb.toString()));
             
-            
-            // 사칙연산 값 저장하기
+            // 사칙연산 계산
             for (int j = 1; j < i; j++) {
-                int k = i-j; // 괄호 계산과 모든 경우의 수 계산
-                
-                for (int num1 : dp.get(j)) {
-                    for (int num2 : dp.get(k)) {
-                        dp.get(i).add(num1 - num2);
-                        dp.get(i).add(num1 + num2);
-                        dp.get(i).add(num1 * num2);
-                        if (num2 != 0) {
-                            dp.get(i).add(num1 / num2);
+                int k = i-j;
+                for (Integer a : dp.get(j)) {
+                    for (Integer b : dp.get(k)) {
+                        dp.get(i).add(a-b);
+                        dp.get(i).add(a+b);
+                        dp.get(i).add(a*b);
+                        // 0 나누기 방지
+                        if (b != 0) {
+                            dp.get(i).add(a/b);
                         }
                     }
                 }
-                
             }
             
             if (dp.get(i).contains(number)) {
                 return i;
             }
         }
+        
         return answer;
     }
 }
