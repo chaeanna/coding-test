8af570aa48e233cefe6fe7f1a197e9af4d8da085
Anna <76768480+chaeanna@users.noreply.github.com>
2024-05-02 13:44:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.57 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class Point {
     int x, y;
     public Point(int x, int y) {
@@ -7,45 +8,43 @@ class Point {
     }
 }
 class Solution {
-    static int[][] answer, map;
-    static boolean[][] visited;
-    static int[] disX = {1, -1, 0, 0};
-    static int[] disY = {0, 0, 1, -1};
-    static int n, m;
+    static int[] disX = {0, 0, 1, -1};
+    static int[] disY = {1, -1, 0, 0};
+    static int[][] visited;
+    static int[][] map;
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        map = maps;
-        answer = new int[n][m];
-        visited = new boolean[n][m];
-        visited[0][0] = true;
-        BFS(0, 0);
-        if (answer[n-1][m-1] == 0) {
-            return -1;
-        }
-        return answer[n-1][m-1];
+        int answer = 0;
+        visited = new int[maps.length][maps[0].length];
+        BFS(maps, 0, 0);
+        answer = visited[maps.length-1][maps[0].length-1];
+        return answer == 0 ? -1 : answer;
     }
     
-    public static void BFS(int x, int y) {
+    public static void BFS(int[][] maps, int x, int y) {
         Queue<Point> q = new LinkedList<>();
         q.offer(new Point(x, y));
-        answer[0][0] = 1;
+        visited[x][y] = 1;
         
         while (!q.isEmpty()) {
             Point p = q.poll();
             for (int i = 0; i < 4; i++) {
+                // 이동할 위치
                 int nx = p.x + disX[i];
                 int ny = p.y + disY[i];
                 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m
-                    && map[nx][ny] == 1 && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    answer[nx][ny] = answer[p.x][p.y] + 1;
+                if (validLoc(nx, ny) 
+                    && maps[nx][ny] == 1
+                    && visited[nx][ny] == 0) {
+                    visited[nx][ny] = visited[p.x][p.y] + 1;
                     q.offer(new Point(nx, ny));
                 }
+                
             }
         }
+        
     }
     
-    
+    public static boolean validLoc(int x, int y) {
+        return x >= 0 && x < visited.length && y >= 0 && y < visited[0].length;
+    }
 }
